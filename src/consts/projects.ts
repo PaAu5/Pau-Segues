2ae7f9f5ac92f7ace5f0407f73e393d70dc7135d
Pau Segués Vitutia <psv.segues@gmail.com>
2025-07-17 12:22:06 +0200

    add link into Solsonaexperience.com

--- src/consts/projects.ts
+++ src/consts/projects.ts
@@ -5,7 +5,7 @@ export const PROJECTS: Project[] = [
         id: 1,
         title: "Solsona Experience Web",
         text: "Official website of Solsona Experience, a tourism cooperative organization",
-        url: "#",
+        url: "https://solsonaexperience.com/",
         image: {
             src: "./images/solsona-experience.webp",
             alt: "Web of Project 1",
